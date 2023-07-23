9ad825ba294b78065946e79e03d7415592d6fb86
phuhl <git@ph-uhl.com>
2023-07-23 12:33:43 +0200

    Adds ts setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,11 +15,33 @@ module.exports = {
         jest: true,
       },
     },
+    {
+      files: ["*.ts"],
+      plugins: ["@typescript-eslint", "jest"],
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+      ],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "tsconfig.json",
+        sourceType: "module",
+      },
+      rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+      },
+    },
   ],
   rules: {
     "no-var": "error",
     "prefer-const": "error",
     "no-unneeded-ternary": "error",
     "prefer-arrow-callback": "error",
+    "no-lonely-if": "error",
+    "consistent-return": ["error", { treatUndefinedAsUnspecified: false }],
+    curly: "error",
+    "@typescript-eslint/no-explicit-any": "off",
   },
 };
--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,17 @@ const jestConfig = require("@apparts/backend-test").getJestConfig();
 
 module.exports = {
   ...jestConfig,
-  // additional config
+  testRunner: "jest-circus/runner",
+  testTimeout: 5000,
+  clearMocks: true,
+  coveragePathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/tests"],
+  watchPathIgnorePatterns: ["/\\.#.*", "/.*~", "/#.*#"],
+
+  testEnvironment: "node",
+  transform: {
+    //    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.[tj]s$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+  testPathIgnorePatterns: ["build", "node_modules"],
 };
