ecd9adf4232094ce9db73be60696e5ae29058e4d
phuhl <git@ph-uhl.com>
2025-04-22 09:40:54 +0200

    Updates dependencies to newest @apparts/types@10

--- src/tests/model.ts
+++ src/tests/model.ts
@@ -1,18 +1,21 @@
 import * as types from "@apparts/types";
-import { BaseModel, useModel } from "@apparts/model";
+import { BaseModel, useModel, ConstructorParams } from "@apparts/model";
 
 const typeSchema = types.obj({
   id: types.int().semantic("id").key().auto().public(),
   optionalVal: types.string().optional().public(),
   hasDefault: types.int().default(7),
   mapped: types.int().mapped("someNumber").public(),
-  isDerived: types
-    .int()
-    .semantic("id")
-    .public()
-    .derived(({ id }) => id),
+  isDerived: types.int().semantic("id").public().derived(),
 });
 
-export class Models extends BaseModel<typeof typeSchema> {}
+export class Models extends BaseModel<typeof typeSchema> {
+  constructor(...args: ConstructorParams<typeof typeSchema>) {
+    super(...args);
+    this.derived({
+      isDerived: ({ id }) => id,
+    });
+  }
+}
 
 useModel(Models, { typeSchema, collection: "model" });
